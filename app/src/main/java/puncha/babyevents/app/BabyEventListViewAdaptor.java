f821c521e6d698b4b48b16bbfdbca0c98fcd2206
PunCha Feng <PunChaPunCha@gmail.com>
2014-06-01 23:28:02 +0800

    Perf Tuning: Use ViewHolder to reuse Views

--- app/src/main/java/puncha/babyevents/app/BabyEventListViewAdaptor.java
+++ app/src/main/java/puncha/babyevents/app/BabyEventListViewAdaptor.java
@@ -12,23 +12,38 @@ import java.util.List;
 import puncha.babyevents.app.db.BabyEventModel;
 import puncha.babyevents.app.db.BabyEventTypes;
 
+
 public class BabyEventListViewAdaptor extends ArrayAdapter<BabyEventModel> {
+    class ViewHolder {
+        public TextView timeView;
+        public TextView quantityView;
+        public TextView typeView;
+    }
+
     public BabyEventListViewAdaptor(Context context, List<BabyEventModel> objects) {
         super(context, R.layout.view_baby_event_item, R.id.time, objects);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
-        TextView timeEdit = (TextView) view.findViewById(R.id.time);
-        TextView quantityEdit = (TextView) view.findViewById(R.id.quantity);
-        TextView typeEdit = (TextView) view.findViewById(R.id.type);
+        ViewHolder viewHolder = new ViewHolder();
+        if (convertView == null) {
+            convertView = super.getView(position, convertView, parent);
+            viewHolder.timeView = (TextView) convertView.findViewById(R.id.time);
+            viewHolder.quantityView = (TextView) convertView.findViewById(R.id.quantity);
+            viewHolder.typeView = (TextView) convertView.findViewById(R.id.type);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
         BabyEventModel item = getItem(position);
-        timeEdit.setText(new SimpleDateFormat("HH:mm").format(item.date()));
-        quantityEdit.setText(String.valueOf(item.quantity()));
-        typeEdit.setText(BabyEventTypes.getResIdForType(item.type()));
+        viewHolder.timeView.setText(new SimpleDateFormat("HH:mm").format(item.date()));
+        viewHolder.quantityView.setText(String.valueOf(item.quantity()));
+        viewHolder.typeView.setText(BabyEventTypes.getResIdForType(item.type()));
 
-        return view;
+        return convertView;
     }
+
 }
